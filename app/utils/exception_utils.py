a1d0020c123202a17ace100757b3c77757eb1bf1
JasonYou <jasonyou.info@gmail.com>
2024-10-13 22:18:59 +0800

    update docker

--- app/utils/exception_utils.py
+++ app/utils/exception_utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import log
 import traceback
 
 
 class ExceptionUtils:
     @classmethod
     def exception_traceback(cls, e):
-        print(f"\nException: {str(e)}\nCallstack:\n{traceback.format_exc()}\n")
+        log.info(f"\nException: {str(e)}\nCallstack:\n{traceback.format_exc()}\n")
